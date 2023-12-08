1dc1ca010c7933625805ce327984f97046e30816
yac0928 <kangarooinapark@gmail.com>
2023-12-08 12:05:26 +0800

    feat: user model add is_admin

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const authenticated = (req, res, next) => {
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    return next()
+  }
+  req.flash('error_messages', '尚未登入')
+  res.redirect('/signin')
+}
+const authenticatedAdmin = (req, res, next) => {
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    if (getUser(req).isAdmin) return next()
+    req.flash('error_messages', '權限不足')
+    // 這裡如果redirect'/'，會再redirect到'/restaurants/，flash僅會在'/'出現，故須直接導向'/restaurants'
+    res.redirect('/restaurants')
+  } else {
+    req.flash('error_messages', '尚未登入')
+    res.redirect('/signin')
+  }
+}
+module.exports = {
+  authenticated,
+  authenticatedAdmin
+}
