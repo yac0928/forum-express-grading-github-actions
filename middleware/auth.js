3e9b8c3c8abbb198ad2135927e0409b2f11d1161
yac0928 <kangarooinapark@gmail.com>
2023-12-13 12:55:12 +0800

    feat: add table-switch partial

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 const authenticated = (req, res, next) => {
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   req.flash('error_messages', '尚未登入')
@@ -9,8 +9,8 @@ const authenticated = (req, res, next) => {
 }
 const authenticatedAdmin = (req, res, next) => {
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next()
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) return next()
     req.flash('error_messages', '權限不足')
     // 這裡如果redirect'/'，會再redirect到'/restaurants/，flash僅會在'/'出現，故須直接導向'/restaurants'
     res.redirect('/restaurants')
