3240f6ad807a20fde5dc2e36bd3ce65fd2174b2b
yac0928 <kangarooinapark@gmail.com>
2023-12-19 23:38:07 +0800

    add dashboard to restaurant
    feat: add user profile viewing
    
    feat: add-image-to-users migration
    
    feat: add user-edit viewing
    
    feat: add R01.test.js and unit-test-helpers.js
    
    add R02.test.js file
    
    chore: init github workflow
    
    Update unit-test-helper.js update()
    
    feat: add put-user function
    
    Pass test-R02
    
    feat: improve R02
    
    improve R02
    
    feat: add R01.test.js and unit-test-helpers.js
    
    add R02.test.js file
    
    correcting typo in comment
    
    chore: init github workflow
    
    Update unit-test-helper.js update()
    
    Delete .travis.yml
    
    feat:add R02.test.js
    
    add R03.test.js file
    
    correcting typo in comment
    
    chore: init github workflow
    
    Delete .travis.yml

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -45,9 +45,21 @@ const restaurantController = {
       ]
     })
       .then(restaurant => {
-        console.log(restaurant.Comments)
         if (!restaurant) throw new Error('Restaurant didn\'t exist')
-        res.render('restaurant', { restaurant: restaurant.toJSON() })
+        return restaurant.increment('viewCount')
+      })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category],
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Comment } = db
+const { localFileHandler } = require('../helpers/file-helpers')
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -35,6 +37,51 @@ const userController = {
     req.flash('success_messages', '登出成功!')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error('User didn\'t exist')
+        res.render('users/profile', { paramUser: user.toJSON() })
+      // 傳入的名稱不能取名為user，否則req.locals.user會被覆蓋，header的部分會出錯
+      })
+  },
+  editUser: (req, res, next) => {
+    const paramId = Number(req.params.id)
+    const userId = Number(req.user.id)
+    if (paramId !== userId) {
+      req.flash('error_messages', '只能編輯自己的profile')
+      res.redirect(`/users/${paramId}`)
+    }
+    return User.findByPk(paramId)
+      .then(user => {
+        if (!user) throw new Error('User didn\'t exist')
+        res.render('users/edit', { user: user.toJSON() })
+      })
+  },
+  putUser: (req, res, next) => {
+    const paramId = Number(req.params.id)
+    const userId = Number(req.user.id)
+    if (paramId !== userId) {
+      res.redirect(`/users/${paramId}`)
+    }
+    const { file } = req
+    return Promise.all([
+      User.findByPk(paramId),
+      localFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error('User didn\'t exist')
+        return user.update({
+          name: req.body.name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${paramId}`)
+      })
+      .catch(err => next(err))
   }
 }
 
