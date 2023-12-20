cd0641cabccdf41cab01d641f4641b9dd2d14563
yac0928 <kangarooinapark@gmail.com>
2023-12-20 20:19:26 +0800

    feat: improve R02

--- models/restaurant.js
+++ models/restaurant.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Restaurant',
-    tableName: 'Restaurants', // 新增
+    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
