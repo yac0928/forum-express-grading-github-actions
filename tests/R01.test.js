f5866da9b2a0848f1a4466e4b8da9dd9937b01d8
yac0928 <kangarooinapark@gmail.com>
2023-12-21 15:22:01 +0800

    feat: add commentedRestaurants list in profile.hbs
    feat: add comments-seed-file
    
    feat: add R01.test.js and unit-test-helpers.js
    
    add R02.test.js file
    
    correcting typo in comment
    
    chore: init github workflow
    
    Update unit-test-helper.js update()
    
    Delete .travis.yml
    
    feat:add R02.test.js
    
    add R03.test.js file
    
    correcting typo in comment
    
    chore: init github workflow
    
    Delete .travis.yml
    
    feat: rename local.passportUser
    
    Pass test-R03

--- tests/R01.test.js
+++ tests/R01.test.js
@@ -107,9 +107,9 @@ describe('# R01', () => {
 
         // patchUser 正確執行的話，應呼叫 req.flash
         // req.flash 的參數應該要與下列字串一致
-        req.flash.calledWith('error_messages', '禁止變更 root 權限').should.be.true
+        req.flash.calledWith('error_messages','禁止變更 root 權限').should.be.true
 
-        // patchUser 執行完畢，應呼叫 res.redirect 並重新導向上一頁
+        // patchUser 執行完畢，應呼叫 res.redirect 並重新導向上一頁 
         res.redirect.calledWith('back').should.be.true
       })
     })
@@ -140,9 +140,9 @@ describe('# R01', () => {
         // 測試作業指定的 adminController.patchUser 函式
         await this.adminController.patchUser(req, res, next)
 
-        // patchUser 正確執行的話，應呼叫 req.flash
+        // patchUser 正確執行的話，應呼叫 req.flash 
         // req.flash 的參數應與下列字串一致
-        req.flash.calledWith('success_messages', '使用者權限變更成功').should.be.true
+        req.flash.calledWith('success_messages','使用者權限變更成功').should.be.true
         // patchUser 執行完畢，應呼叫 res.redirect 並重新導向 /admin/users
         res.redirect.calledWith('/admin/users').should.be.true
 
@@ -179,9 +179,9 @@ describe('# R01', () => {
         // 測試作業指定的 adminController.patchUser 函式
         await this.adminController.patchUser(req, res, next)
 
-        // patchUser 正確執行的話，應呼叫 req.flash
+        // patchUser 正確執行的話，應呼叫 req.flash 
         // req.flash 的參數應與下列字串一致
-        req.flash.calledWith('success_messages', '使用者權限變更成功').should.be.true
+        req.flash.calledWith('success_messages','使用者權限變更成功').should.be.true
         // patchUser 執行完畢，應呼叫 res.redirect 並重新導向 /admin/users
         res.redirect.calledWith('/admin/users').should.be.true
 
