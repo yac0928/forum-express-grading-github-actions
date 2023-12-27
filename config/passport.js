4a23b2ccaa01dd495cfc1bd1d39e47ef38386b28
yac0928 <kangarooinapark@gmail.com>
2023-12-27 10:01:01 +0800

    feat: add topUser page

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,7 @@ passport.deserializeUser((id, cb) => {
     include: [
       { model: Restaurant, as: 'FavoritedRestaurants' },
       { model: Restaurant, as: 'LikedRestaurants' },
-      { model: User, as: 'Followers' }
+      { model: User, as: 'Followers' },
       { model: User, as: 'Followings' }
     ]
   })
