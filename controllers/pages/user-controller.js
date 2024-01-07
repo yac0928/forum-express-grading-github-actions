8b47565b525d3498e61e1bddea249649e6310468
yac0928 <kangarooinapark@gmail.com>
2024-01-07 15:43:37 +0800

    feat: add signUp api

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,29 +1,35 @@
-const bcrypt = require('bcryptjs')
 const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../../models')
 const { localFileHandler } = require('../../helpers/file-helpers')
+const userServices = require('../../services/user-services')
 
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    userServices.signUp(req, (err, data) => {
+      if (err) return next(err)
+      req.flash('success_messages', '成功註冊帳號!')
+      req.session.createdData = data
+      res.redirect('/signin')
+    })
+    // if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
 
-    return User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10)
-      })
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => {
-        req.flash('success_messages', '成功註冊帳號!')
-        res.redirect('/signin')
-      })
-      .catch(err => next(err))
+    // return User.findOne({ where: { email: req.body.email } })
+    //   .then(user => {
+    //     if (user) throw new Error('Email already exists!')
+    //     return bcrypt.hash(req.body.password, 10)
+    //   })
+    //   .then(hash => User.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: hash
+    //   }))
+    //   .then(() => {
+    //     req.flash('success_messages', '成功註冊帳號!')
+    //     res.redirect('/signin')
+    //   })
+    //   .catch(err => next(err))
   },
   signInPage: (req, res) => {
     res.render('signin')
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -12,6 +12,7 @@ router.use('/admin', authenticated, authenticatedAdmin, admin)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.post('/signup', userController.signUp)
 
 router.use('/', apiErrorHandler)
 
